234686bcc2774ff5697980c90c1577f11c52527a
Alison8547 <alisonailson100@hotmail.com>
2023-09-01 15:22:33 -0300

    feat: project base

---
+++ src/main/java/com/br/springjpapractices/SpringJpaPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.br.springjpapractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaPracticesApplication.class, args);
+	}
+
+}
